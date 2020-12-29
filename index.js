38da9c766995bb7adddbe1698cd65d210fb318e3
sunbot977 <cjsdntjr5@gmail.com>
2020-12-29 23:30:09 +0900

    bot upload

--- index.js
+++ index.js
@@ -24,9 +24,8 @@ const roleName = "게스트" // 입장 시 지급 할 역할의 이름을 적어
 client.on("ready", () => {
   console.log("켰다.")
 
-  client.user.setActivity("TWITCH", { type: "STREAMING", url: "https://www.twitch.tv/sangmin4097"});
   client.user.setPresence({ activity: { name: "구매문의 : 총관리자 OR 티켓" }})
-
+  client.user.setActivity("TWITCH", { type: "STREAMING", url: "https://www.twitch.tv/sangmin4097"});
   let state_list = [
     "구매문의 : 총관리자 OR 티켓",
     "?도움말을 입력해보세요.",
