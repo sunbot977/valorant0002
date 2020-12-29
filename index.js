025c4fc360c6ecdcd8a7a49287dd9d78ec3484cf
sunbot977 <cjsdntjr5@gmail.com>
2020-12-29 23:56:59 +0900

    bot upload

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ client.on("ready", () => {
     setTimeout(() => {
       console.log( "상태 변경 -> ", state_list[state_list_index] );
       client.user.setPresence({ activity: { name: state_list[state_list_index] } })
-      client.user.setActivity("", { type: "STREAMING", url: "https://www.twitch.tv/sangmin4097"});
+      client.user.setActivity({ type: "STREAMING", url: "https://www.twitch.tv/sangmin4097"});
       state_list_index += 1;
       if(state_list_index >= state_list.length) {
         state_list_index = 0;
