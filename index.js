6ec1739718ff57ffbc34d5dc43d27add46af434f
sunbot977 <cjsdntjr5@gmail.com>
2020-12-30 23:26:23 +0900

    bot upload

--- index.js
+++ index.js
@@ -411,7 +411,7 @@ client.on("message", (message) => {
   let embed = new Discord.MessageEmbed()
     //.addBlankField()  < 해당 구문은 .addField('\u200b', '\u200b') 로 대체할 수 있습니다.
     .addField("**`구매문의`**","**__총관리자 or 티켓__**")
-    .addField("**`가격`**","**__이벤트 가격 : 1일 17,500 KRW__**\n")
+    .addField("**`가격`**","**__1일 : 17,500 KRW__**\n")
     .addField("**`구동영상`**","**__데스매치 플레이 영상__**\nhttps://streamable.com/nm28j7\n")
     .addField("**`구동영상 2`**","**__데스매치 플레이 영상 2__**\nhttps://streamable.com/nz8q33\n")
     //.addBlankField()  < 해당 구문은 .addField('\u200b', '\u200b') 로 대체할 수 있습니다.
